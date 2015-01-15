cd8b7592c44921ccb5bb2f4f2c0f2bfe33804009
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 15:21:44 -0800

    refactors link model/schema

--- app/models/link.js
+++ app/models/link.js
@@ -1,24 +1,25 @@
 var db = require('../config');
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+
+
+var linkSchema = mongoose.Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: Number
+});
 
 var Link = mongoose.model('Link', db.linkSchema);
 
+
+linkSchema.pre('save', function(next){
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
+
+
 module.exports = Link;
-
-//var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function(){
-//     this.on('creating', function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-
